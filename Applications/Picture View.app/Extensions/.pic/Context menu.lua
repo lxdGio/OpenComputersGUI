d1b736b5f1010136f1ecef357d9a5546217e345d
lxdGio <ihatenvidia-double@proton.me>
2025-02-26 02:58:23 +0500

    Delete Applications directory
    Nax

--- Applications/Picture View.app/Extensions/.pic/Context menu.lua
+++
@@ -1,6 +0,0 @@
-local system = require("System")
-
-local workspace, icon, menu = table.unpack({...})
-
-system.addSetAsWallpaperMenuItem(menu, icon.path)
-system.addUploadToPastebinMenuItem(menu, icon.path)
